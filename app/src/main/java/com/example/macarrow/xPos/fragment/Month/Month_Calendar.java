1189f4ba74b034054d69d74464fe58567729b521
PetitpapaDev <diviner85@gmail.com>
2017-07-13 13:59:26 +0900

    layout

--- app/src/main/java/com/example/macarrow/xPos/fragment/Month/Month_Calendar.java
+++ app/src/main/java/com/example/macarrow/xPos/fragment/Month/Month_Calendar.java
@@ -1,21 +1,35 @@
 package com.example.macarrow.xPos.fragment.Month;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ListView;
 import android.widget.TextView;
 import com.example.macarrow.xPos.DayInfo;
 import com.example.macarrow.xPos.R;
-import com.example.macarrow.xPos.adapter.MonthCalendarViewAdapter;
+import com.example.macarrow.xPos.Services.Month_Service;
 import com.example.macarrow.xPos.fragment.Month_Fragment;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 public class Month_Calendar extends Fragment implements View.OnClickListener {
 
@@ -96,12 +110,7 @@ public class Month_Calendar extends Fragment implements View.OnClickListener {
                 month = mThisMonthCalendar.get(Calendar.MONTH)+1;
                 mDay = Integer.parseInt(day.getDay());
 
-                Bundle args = new Bundle();
-                args.putInt("year", year);
-                args.putInt("month", month);
-                args.putInt("day", mDay);
                 Month_inNout month_inNout = new Month_inNout();
-                month_inNout.setArguments(args);
                 month_inNout.setCancelable(false);
                 month_inNout.show(getFragmentManager(), "month_inNout");
 
@@ -206,4 +215,215 @@ public class Month_Calendar extends Fragment implements View.OnClickListener {
         mAdapter = new MonthCalendarViewAdapter(getActivity(), R.layout.month_calendar_view, mDayList);
         CalGrid.setAdapter(mAdapter);
     }
+
+    public class MonthCalendarViewAdapter extends BaseAdapter {
+
+        private ArrayList<DayInfo> mDayList;
+        private Context context;
+        private int mResource;
+        private LayoutInflater mflater;
+
+        public MonthCalendarViewAdapter (Context context, int textResource, ArrayList<DayInfo> dayList) {
+            this.context = context;
+            this.mDayList = dayList;
+            this.mResource = textResource;
+            this.mflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public int getCount() {
+            return mDayList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mDayList.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView (int position, View convertView, ViewGroup parent) {
+
+            DayInfo day = mDayList.get(position);
+            MonthCalendarView_Item dayViewHolder;
+
+            if (convertView == null) {
+
+                convertView = mflater.inflate(mResource, null);
+                dayViewHolder = new MonthCalendarView_Item();
+                dayViewHolder.Calendar_day = (TextView) convertView.findViewById(R.id.calendar_day);
+                dayViewHolder.Month_in_cnt = (TextView)convertView.findViewById(R.id.month_in_cnt);
+                dayViewHolder.Month_out_cnt = (TextView)convertView.findViewById(R.id.month_out_cnt);
+                convertView.setTag(dayViewHolder);
+
+            } else {
+
+                dayViewHolder = (MonthCalendarView_Item) convertView.getTag();
+
+            } if (day != null) {
+
+                Calendar mCal = Calendar.getInstance();
+                Integer today = mCal.get(Calendar.DAY_OF_MONTH);
+                String sToday = String.valueOf(today);
+                dayViewHolder.Calendar_day.setText(day.getDay());
+
+                long now = System.currentTimeMillis();
+                final Date date = new Date(now);
+                final SimpleDateFormat curYearFormat = new SimpleDateFormat("yyyy", Locale.KOREA);
+                final SimpleDateFormat curMonthFormat = new SimpleDateFormat("MM", Locale.KOREA);
+                final SimpleDateFormat curDayFormat = new SimpleDateFormat("dd", Locale.KOREA);
+
+                Month_Service month_service = new Month_Service(context, "month.db", null, 1);
+                int year = Integer.parseInt(curYearFormat.format(date));
+                int month = Integer.parseInt(curMonthFormat.format(date));
+                int tDay = Integer.parseInt(curDayFormat.format(date));
+
+                int in = month_service.calMonthInCnt(year, month, tDay);
+                int out = month_service.calMonthOutCnt(year, month, tDay);
+
+                if (day.getDay() == sToday) {
+                    dayViewHolder.Month_in_cnt.setText("시작"+in+"대");
+                    dayViewHolder.Month_out_cnt.setText("종료"+out+"대");
+                }
+
+                if (day.isInMonth()) {
+
+                    if (day.getDay() == sToday) {
+                        dayViewHolder.Calendar_day.setTextColor(Color.GREEN);
+                    } else if (position % 7 == 0) {
+                        dayViewHolder.Calendar_day.setTextColor(Color.RED);
+                    } else if (position % 7 == 6) {
+                        dayViewHolder.Calendar_day.setTextColor(Color.BLUE);
+                    }
+
+                } else {
+                    dayViewHolder.Calendar_day.setText("");
+                }
+
+            }
+            return convertView;
+        }
+    }
+
+    public class MonthinNoutViewAdapter extends BaseAdapter {
+
+        private Context context;
+        private int layout;
+        private List<Map<String, Object>> list;
+        private LayoutInflater inflater;
+
+        public MonthinNoutViewAdapter(Context context, int layout, List<Map<String, Object>> list) {
+
+            this.context = context;
+            this.layout = layout;
+            this.list = list;
+
+            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            MonthinNoutView_Item item = null;
+
+            if (convertView == null) {
+                convertView = inflater.inflate(layout, null);
+                TextView Month_num = (TextView) convertView.findViewById(R.id.month_num);
+                TextView Month_status = (TextView) convertView.findViewById(R.id.month_status);
+
+                item = new MonthinNoutView_Item();
+                item.monthNum = Month_num;
+                item.monthStatus = Month_status;
+
+                convertView.setTag(item);
+
+            } else {
+                item = (MonthinNoutView_Item) convertView.getTag();
+            }
+
+            int start_date_y = (int) list.get(position).get("start_date_y");
+            int start_date_m = (int) list.get(position).get("start_date_m");
+            int start_date_d = (int) list.get(position).get("start_date_d");
+            int end_date_y = (int) list.get(position).get("end_date_y");
+            int end_date_m = (int) list.get(position).get("end_date_m");
+            int end_date_d = (int) list.get(position).get("end_date_d");
+
+            item.monthNum.setText((String) list.get(position).get("car_num"));
+
+            if (start_date_y == year && start_date_m == month &&start_date_d == mDay) {
+                item.monthStatus.setText("시작");
+            }
+
+            if (end_date_y == year && end_date_m == month &&end_date_d == mDay) {
+                item.monthStatus.setText("종료");
+            }
+
+            return convertView;
+        }
+
+        @Override
+        public int getCount() {
+            return list.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+    }
+
+    public class Month_inNout extends DialogFragment {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            final Month_Service month_service = new Month_Service(getActivity(), "month.db", null, 1);
+            LayoutInflater inflater = getActivity().getLayoutInflater();
+
+            final View view = inflater.inflate(R.layout.month_innout, null);
+            final ListView Month_inNouts = (ListView) view.findViewById(R.id.month_inNout);
+
+            final List<Map<String, Object>> list = month_service.calMonth(year, month, mDay);
+            MonthinNoutViewAdapter adapter = new MonthinNoutViewAdapter(getActivity(), R.layout.month_innout_item, list);
+            Month_inNouts.setAdapter(adapter);
+
+            builder.setNegativeButton("닫기", null);
+            builder.setView(view);
+
+            return builder.create();
+        }
+        @Override
+        public void onResume() {
+            super.onResume();
+            Window window = getDialog().getWindow();
+            window.setLayout(835, WindowManager.LayoutParams.WRAP_CONTENT);
+        }
+    }
+}
+
+class MonthCalendarView_Item {
+
+    TextView Calendar_day;
+    TextView Month_in_cnt;
+    TextView Month_out_cnt;
+
+}
+
+class MonthinNoutView_Item {
+
+    TextView monthNum;
+    TextView monthStatus;
+
 }
